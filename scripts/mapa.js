5076430e4012a39bf549c81c0650d7ade767aa0d
Renzo Perez <renzin@gmail.com>
2022-04-26 12:20:40 -0500

    first commit

---
+++ scripts/mapa.js
@@ -0,0 +1,11 @@
+function iniciarMap(){
+  var coord = {lat:-12.0778278, lng: -77.071941};
+  var map = new google.maps.Map(document.getElementById('map'),{
+    zoom: 10,
+    center: coord
+  });
+  var marker = new google.maps.Marker({
+    position: coord,
+    map: map
+  });
+}
